80caf38ea0f0882ae88fdb48431f3a32e0e9d92c
Fabio Vilas Boas Machado <fabio.machado@grupocrm.local>
2020-04-14 22:48:33 -0300

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fabio.machado\pycharmprojects\projetofinal\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
