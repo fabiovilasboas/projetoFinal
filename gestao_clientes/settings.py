8170b79c39926956346067d1848e3a7a20c7623e
Fabio Vilas Boas Machado <fabio.machado@grupocrm.local>
2020-04-14 22:53:36 -0300

    Fix Error

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes62.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['gestao-clientes52.herokuapp.com', 'localhost']
 
 
 # Application definition
