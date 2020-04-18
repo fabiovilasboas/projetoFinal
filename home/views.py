04723314956e1799cdabbc7cbb9de0758e6acd8a
fabio.vbm@gmail.com <Icasa@0362>
2020-04-18 17:47:40 -0300

    Atualização do Requiments

--- home/views.py
+++ home/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
-
+#TODO: Teste do todo
 def home(request):
-    return render(request, 'home.html')
-
+    value1 = 10
+    value2 = 20
+    res = value1 * value2
+    return render(request, 'home.html', {'result': res})
+#FIXME: Teste do fixme
 def my_logout(request):
     logout(request),
     return redirect('home')
